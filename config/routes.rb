12fca81b3849244db0a270d47346c7c55aa902d5
Louis Leslie <louis.leslie@gmail.com>
2022-08-23 14:14:32 +0100

    Complete CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "/restaurants", to: "restaurants#index"
+  # post "/restaurants", to: "restaurants#create"
+  # get "restaurants/new", to: "restaurants#new"
+  # get "/restaurants/:id", to: "restaurants#show", as: :restaurant
+  # delete "/restaurants/:id", to: "restaurants#destroy"
+  # # put "/restaurants/:id"
+  # get "/restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+
+  resources :restaurants
 end
